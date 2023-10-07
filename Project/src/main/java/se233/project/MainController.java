ec27c3141b9b39212e449a23a380c02109f04b2d
Ktok-CMU <119600502+Ktok-CMU@users.noreply.github.com>
2023-10-07 10:04:40 +0700

    Add files via upload

--- Project/src/main/java/se233/project/MainController.java
+++ Project/src/main/java/se233/project/MainController.java
@@ -2,6 +2,7 @@ package se233.project;
 
 import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
@@ -31,7 +32,19 @@ public class MainController {
     @FXML
     private Label resultLabel;
 
+    @FXML
+    private CheckBox keepRatioCheckbox;
+
     private Image importedImage;
+    private Image previewImage;
+
+    private boolean keepAspectRatio = true;
+
+    @FXML
+    public void initialize() {
+        // Initialize the checkbox to be selected by default
+        keepRatioCheckbox.setSelected(true);
+    }
 
     @FXML
     public void resizeImage() {
@@ -40,9 +53,9 @@ public class MainController {
         String heightStr = heightField.getText();
         String percentStr = percentField.getText();
 
-        // Convert the input to integers
-        int width = Integer.parseInt(widthStr);
-        int height = Integer.parseInt(heightStr);
+        // Convert the input to doubles
+        double width = Double.parseDouble(widthStr);
+        double height = Double.parseDouble(heightStr);
 
         // Initialize the percentage value with 100% (no change by default)
         double percent = 100.0;
@@ -57,11 +70,11 @@ public class MainController {
             BufferedImage bufferedImage = SwingFXUtils.fromFXImage(importedImage, null);
 
             // Adjust width and height based on the percentage
-            width = (int) (width * percent / 100.0);
-            height = (int) (height * percent / 100.0);
+            width = width * percent / 100.0;
+            height = height * percent / 100.0;
 
-            BufferedImage resizedImage = new BufferedImage(width, height, bufferedImage.getType());
-            resizedImage.createGraphics().drawImage(bufferedImage, 0, 0, width, height, null);
+            BufferedImage resizedImage = new BufferedImage((int) width, (int) height, bufferedImage.getType());
+            resizedImage.createGraphics().drawImage(bufferedImage, 0, 0, (int) width, (int) height, null);
 
             importedImage = SwingFXUtils.toFXImage(resizedImage, null);
             importedImageView.setImage(importedImage);
@@ -84,6 +97,14 @@ public class MainController {
             // Load the selected image
             importedImage = new Image(selectedFile.toURI().toString());
             importedImageView.setImage(importedImage);
+
+            // Reset the preview image when a new image is loaded
+            previewImage = null;
+
+            // Always keep aspect ratio after importing an image
+            keepAspectRatio = true;
+            keepRatioCheckbox.setSelected(true);
+            updateImageViewAspectRatio();
         }
     }
 
@@ -115,5 +136,74 @@ public class MainController {
             }
         }
     }
-}
 
+    @FXML
+    public void checkBox() {
+        // Toggle the value of keepAspectRatio when the checkbox is clicked
+        keepAspectRatio = keepRatioCheckbox.isSelected();
+
+        // If keepAspectRatio is true, update the aspect ratio of the image view
+        if (keepAspectRatio) {
+            updateImageViewAspectRatio();
+        } else {
+            // Reset the preview image when aspect ratio is not maintained
+            importedImageView.setImage(importedImage);
+            previewImage = null;
+
+            // Clear the width and height fields
+            widthField.clear();
+            heightField.clear();
+
+            // Clear the result label
+            resultLabel.setText("");
+        }
+    }
+
+    private void updateImageViewAspectRatio() {
+        if (importedImage != null) {
+            double originalWidth = importedImage.getWidth();
+            double originalHeight = importedImage.getHeight();
+
+            // Calculate the new width and height to maintain the aspect ratio
+            double newWidth = originalWidth;
+            double newHeight = originalHeight;
+
+            if (!widthField.getText().isEmpty()) {
+                try {
+                    newWidth = Double.parseDouble(widthField.getText());
+                    newHeight = newWidth * (originalHeight / originalWidth);
+                } catch (NumberFormatException e) {
+                    // Handle the exception (e.g., show an error message)
+                    e.printStackTrace();
+                }
+            } else if (!heightField.getText().isEmpty()) {
+                try {
+                    newHeight = Double.parseDouble(heightField.getText());
+                    newWidth = newHeight * (originalWidth / originalHeight);
+                } catch (NumberFormatException e) {
+                    // Handle the exception (e.g., show an error message)
+                    e.printStackTrace();
+                }
+            }
+
+            // Update the width and height fields with the calculated values
+            widthField.setText(String.format("%.2f", newWidth));
+            heightField.setText(String.format("%.2f", newHeight));
+
+            // Create a preview image with the new aspect ratio
+            BufferedImage bufferedImage = SwingFXUtils.fromFXImage(importedImage, null);
+            BufferedImage previewBufferedImage = new BufferedImage((int) newWidth, (int) newHeight, bufferedImage.getType());
+            previewBufferedImage.createGraphics().drawImage(bufferedImage, 0, 0, (int) newWidth, (int) newHeight, null);
+
+            // Convert the preview image to FXImage
+            previewImage = SwingFXUtils.toFXImage(previewBufferedImage, null);
+
+            // Display the preview image
+            importedImageView.setImage(previewImage);
+
+            // Update the resultLabel with aspect ratio information
+            String result = "Aspect ratio maintained: Width = " + newWidth + ", Height = " + newHeight;
+            resultLabel.setText(result);
+        }
+    }
+}
